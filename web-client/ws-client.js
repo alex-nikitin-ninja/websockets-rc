1deeeccf2f15c73f8d3489bd4ff9c03c4fe60801
Alex N <alex@nikitin.ninja>
2020-11-24 15:23:20 -0800

    minor update

--- web-client/ws-client.js
+++ web-client/ws-client.js
@@ -1,15 +1,15 @@
-// let wsEnabled = false;
-let wsEnabled = true;
+let wsEnabled = false;
+// let wsEnabled = true;
 
 if (wsEnabled === true) {
-	var ws = new WebSocket("ws://192.168.0.116:8765/");
-	// var ws = new WebSocket("ws://127.0.0.1:8765/");
+	// var ws = new WebSocket("ws://192.168.0.116:8765/");
+	var ws = new WebSocket("ws://127.0.0.1:8765/");
 
 	function sendJStatus(){
 	    ws.send(JSON.stringify(movableObjectValues)); 
-	    // setTimeout(function(){
-	    //     sendJStatus();
-	    // }, 50);
+	    setTimeout(function(){
+	        sendJStatus();
+	    }, 50);
 	};
 
 	ws.onopen = function (event) {
@@ -19,7 +19,7 @@ if (wsEnabled === true) {
 	};
 
 	ws.onmessage = function (event) {
-	    sendJStatus();
+	    // sendJStatus();
 	    console.log(event.data);
 	};
 }
